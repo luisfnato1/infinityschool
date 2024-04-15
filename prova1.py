b68aa57344dc085a0ec8c7888e892b07364e30a4
luisfnato1 <167127998+luisfnato1@users.noreply.github.com>
2024-04-15 15:35:33 -0300

    Add files via upload

---
+++ prova1.py
@@ -0,0 +1,11 @@
+#01 Variaveis
+
+nome = "Luis Fellipe"
+idade = 24 (int)
+altura = 1.71 (float)
+namora = True (bool)
+
+#02 Solicitação de dados + 03 retorno com mensagem personalizada
+
+nome = input("Qual é o seu nome?")
+print (f'Olá {nome}, prazer em te ter por aqui')
